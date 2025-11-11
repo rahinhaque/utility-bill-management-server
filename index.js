d74ef6ef107663c37d1ad6c67bef8b31538bb881
Rahin Haque <haquerahin743@gmail.com>
2025-11-11 19:46:12 +0600

    Add Bills

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const { ObjectId } = require("mongodb");
 
-
-
-
-
-
-
 //middleware
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri =
   "mongodb+srv://utility-bills-server:4JKLEoU7WdwkXQUB@simple-crud-sever.mcwoj3p.mongodb.net/?appName=simple-crud-sever";
@@ -28,10 +20,8 @@ const client = new MongoClient(uri, {
   },
 });
 
-
 async function run() {
   try {
-    
     await client.connect();
 
     //bills collections
@@ -39,52 +29,57 @@ async function run() {
     const billsCollections = db.collection("bills");
     const recentData = db.collection("recent-data");
 
-    app.get("/bills" , async(req, res)=>{
-      try{
+
+    app.post("/bills", async (req, res) => {
+      try {
+        const newBill = req.body;
+        const result = await billsCollections.insertOne(newBill);
+        res.send(result);
+      } catch (error) {
+        console.error("Error adding bill:", error);
+        res.status(500).send({ message: "Failed to add bill" });
+      }
+    });
+
+
+    app.get("/bills", async (req, res) => {
+      try {
         const bills = await billsCollections.find().toArray();
         res.send(bills);
+      } catch {
+        res.status(500).send({ error: Bad_Request });
       }
-      catch{
-        res.status(500).send({error : Bad_Request});
-      }
-    })
+    });
 
     app.get("/recent-data", async (req, res) => {
       try {
-        const data = await recentData.find().toArray();         res.send(data);
+        const data = await recentData.find().toArray();
+        res.send(data);
       } catch (error) {
         res.status(500).send({ error: "Server error" });
       }
     });
 
+    app.get("/recent-data/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await recentData.findOne(query);
+      res.send(result);
+    });
 
-
-  app.get("/recent-data/:id" ,async(req, res)=>{
-    const id = req.params.id;
-    const query = {_id : new ObjectId(id)}
-    const result = await recentData.findOne(query);
-    res.send(result);
-  })
-
-
-   
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-   
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-app.get("/" , (req , res)=> {
+app.get("/", (req, res) => {
   res.send("Utility-Server is runnig");
-})
-
-
+});
 
 app.listen(port, () => {
   console.log(`Utility app listening on port ${port}`);
